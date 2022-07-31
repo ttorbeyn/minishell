a68525b4334de137e987439983be344e83eff9fb
Bob Razovski <vic@MacBook-Pro-de-Bob.local>
2022-07-31 16:34:56 +0200

    debut

--- exec/builtin/exec_exit.c
+++ exec/builtin/exec_exit.c
@@ -1,34 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   exec_env.c                                         :+:      :+:    :+:   */
+/*   exec_exit.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vic <vic@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/30 16:36:41 by vic               #+#    #+#             */
-/*   Updated: 2022/07/31 16:13:48 by vic              ###   ########.fr       */
+/*   Created: 2022/07/31 16:14:59 by vic               #+#    #+#             */
+/*   Updated: 2022/07/31 16:21:20 by vic              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-int	exec_env(t_command command, char **envp)
+int	exec_exit(t_command command)
 {
-	int i;
 
-	i = 0;
-	if (command.ac > 2)
-	{
-		ft_putendl_fd("Error: Too many arguments", 2);
-		return (1);
-	}
-	if (!envp)
-		return (1);
-	while (envp[i])
-	{
-		ft_putstr_fd(envp[i], 1);
-		write (1, "\n", 1);
-		i++;
-	}
-	return (0);
 }
